0cc2b12c728610daf642343952b5b229a79f9d5e
hcbviolet <hcbviolet@gmail.com>
2016-09-23 08:20:11 -0700

    Start arcs

--- app/scripts/fc_data.js
+++ app/scripts/fc_data.js
@@ -1,13 +1,30 @@
 $(document).ready(function() {
     var currentYear = new Date().getFullYear();
+    var year = 1973;
     $.ajax({
-        url: "http://localhost:3000/api/predictions?year=1973",
+        url: "http://localhost:3000/api/predictions?year=" + year,
         headers: {
         'X-Auth-Token' : "xAqO541unK52OdXpnfSGWZUW/c3EZy+ANcMO3rJFepzrhy/p8j7zK6DcFC1J98i35zRsH8hPG2qzbMyDshSBmw==" 
         }
     }).then(function(data) {
         for(var i = 0; i < data.length; i++){
-            console.log(data[i]);
+            if(data[i]){
+                var arcLength = data[i].endyear - data[i].startyear;
+                var point = currentYear - data[i].endyear;
+                var vis = d3.select("body").append("svg");
+                var pi = Math.PI;
+                var arc = d3.svg.arc()
+                    .innerRadius(arcLength)
+                    .outerRadius(arcLength + 10)
+                    .startAngle(.5 * pi) //radians
+                    .endAngle(-.5 * pi) 
+                vis.attr("width", "400").attr("height", "400") // Added height and width so arc is visible
+                    .append("path")
+                    .attr("d", arc)
+                    .attr("fill", "white")
+                    .attr("transform", "translate(200,200)");
+            }
         }
     });
+
 });
