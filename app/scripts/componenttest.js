c6fc0e68d312497a4504de69f8292574f131a83d
hcbviolet <hcbviolet@gmail.com>
2016-12-03 10:07:58 -0800

    Register components

--- app/scripts/componenttest.js
+++ app/scripts/componenttest.js
@@ -1,29 +1,73 @@
-Vue.component('trend', {
-  template: `
-  <div id="trend-instance-container">
-    <label for="trend"><h2>Trend Name</h2></label>
-    <input type="text" name="trend"/>
-  </div>
-  `
-})
-
-Vue.component('occurrence', {
+const authToken = "xAqO541unK52OdXpnfSGWZUW/c3EZy+ANcMO3rJFepzrhy/p8j7zK6DcFC1J98i35zRsH8hPG2qzbMyDshSBmw==";
+const occurrenceTemplate = {
   template: `<div class="instance-container">
-      <label>
-        <span>Name</span><input type="text" name="name" />
-      </label><br>
+        <label>
+          <span>Name</span><input type="text" id="occcurrence" value=""/>
+        </label><br>
 
-      <label>
-        <span>Start Year</span><input type="text" name="startyear" />
-      </label><br>
+        <label>
+          <span>Start Year</span><input type="text" id="startyear" value=""/>
+        </label><br>
 
-      <label>
-        <span>End Year</span><input type="text" name="endyear" />
-      </label><br>
-  </div>
-  `
-})
+        <label>
+          <span>End Year</span><input type="text" id="endyear" value=""/>
+        </label><br>
+    </div>`
+}
 
-new Vue({
-  el: '#trendForm'
+const trendTemplate = {
+  template: `<div id="trend-instance-container">
+    <label for="trend"><h2>Trend Name</h2></label>
+    <input type="text" name="trend" id="trend" value=""/>
+  </div>`
+}
+
+const vm = new Vue({
+  el: '#app',
+  data: {
+    //data?
+  },
+  methods: {
+    addOcc: function(){
+      $('form').append(occurrenceTemplate);
+    },
+    submitData: function(){
+      var trendName = document.getElementById('trend').value;
+      //  DO SOME GODDAMN VALIDATION, HANNAH
+      //        if (!$.trim(trendName)) {
+      //            alert("Trend name is empty");
+      //            return false;
+      //        }
+      var trendId;
+      console.log(JSON.stringify({trend: { name: trendName, user_id: "1" }}));
+      var jqxhr = $.ajax({
+        type: "POST",
+        url: "https://flat-circle-app.herokuapp.com/api/trends",
+        headers: {'X-Auth-Token' : authToken },
+        data: JSON.stringify({trend: { name: trendName, user_id: "1" }}),
+        contentType: "application/json; charset=UTF-8",
+        success: function(response){
+          trendId = response.id;
+          $('.instance-container').each(function(i, obj){
+            var name = obj.getElementsByTagName('input')[0].value;
+            var startYear = obj.getElementsByTagName('input')[1].value;
+            var endYear = obj.getElementsByTagName('input')[2].value;
+            console.log(JSON.stringify({occurrence: { name: name, startYear: startYear, endYear: endYear, trend_id: trendId}}));
+            $.ajax({
+              type: "POST",
+              url: "https://flat-circle-app.herokuapp.com/api/trends/" + trendId + "/occurrences",
+              headers: {'X-Auth-Token' : authToken },
+              data: JSON.stringify({occurrence: { name: name, startYear: startYear, endYear: endYear, trend_id: trendId}}),
+              contentType: "application/json; charset=UTF-8"
+            })
+          })
+        }
+      })
+      console.log(jqxhr);
+    }
+  },
+  components: {
+    'occurrence': occurrenceTemplate,
+    'trend': trendTemplate
+  }
 })
--- app/scripts/fc_viz.js
+++ app/scripts/fc_viz.js
@@ -4,20 +4,20 @@ $(document).ready(function() {
     document.getElementById("year-button").onclick = function (){
         year = document.getElementById("year-input").value;
         $.ajax({
-            url: "http://localhost:3000/api/predictions?year=" + year,
+            url: "https://flat-circle-app.herokuapp.com/api/predictions?year=" + year,
             headers: {
-            'X-Auth-Token' : "xAqO541unK52OdXpnfSGWZUW/c3EZy+ANcMO3rJFepzrhy/p8j7zK6DcFC1J98i35zRsH8hPG2qzbMyDshSBmw==" 
+            'X-Auth-Token' : "xAqO541unK52OdXpnfSGWZUW/c3EZy+ANcMO3rJFepzrhy/p8j7zK6DcFC1J98i35zRsH8hPG2qzbMyDshSBmw=="
             }
         }).then(function(data) {
             var trendData = data.filter(function (trend) {
                 return trend !== null;
             });
-            
+
             trendData.sort(function (a, b) {
                 return (a.endyear - a.startyear) - (b.endyear - b.staryear);
             });
-            
-            
+
+
             console.log(trendData);
             var total = trendData.length;
             var interval = 255 / total;
@@ -34,8 +34,8 @@ $(document).ready(function() {
                     .innerRadius(2*arcLength)
                     .outerRadius(2*(arcLength + 10))
                     .startAngle(.5 * pi)
-                    .endAngle(-.5 * pi) 
-                vis.attr("width", "500").attr("height", "500").attr("viewbox", "0 0 500 500") 
+                    .endAngle(-.5 * pi)
+                vis.attr("width", "500").attr("height", "500").attr("viewbox", "0 0 500 500")
                     .classed("trend-arc", true)
                     .append("path")
                     .attr("d", arc)
