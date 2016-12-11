e9a2d4392e7b54c21103fac48585d069e7719e4d
hcbviolet <hcbviolet@gmail.com>
2016-12-11 09:34:54 -0800

    Basic form validation

--- app/scripts/componenttest.js
+++ app/scripts/componenttest.js
@@ -30,46 +30,70 @@ const trendTemplate = {
 const vm = new Vue({
   el: '#app',
   data: {
-    //data?
+    message: "",
+    numError: `'${value}' is not a valid year`,
+    nameError: `The name '${name}' is invalid`,
+    unknownError: `Sorry buddy! Something went wrong. Please check your form and try again?`
   },
   methods: {
+    validateNum: function(value) {
+      var valid = !isNaN(value) &&
+             parseInt(Number(value)) == value &&
+             !isNaN(parseInt(value, 10));
+      if(valid){
+        return valid;
+      } else if(!valid) {
+        this.message = this.numError;
+      } else {
+        this.message = this.unknownError;
+      }
+    },
+    validateName: function(name) {
+      var valid = !(!$.trim(name)) && name.length <= 32;
+      if (valid) {
+        return valid;
+      } else if (!valid) {
+        this.message = this.nameError;
+      } else {
+        this.message = this.unknownError;
+      }
+    },
     addOcc: function(){
       $('form').append(occurrenceTemplate);
     },
     submitData: function(){
       var trendName = document.getElementById('trend').value;
-      //  DO SOME GODDAMN VALIDATION, HANNAH
-      //        if (!$.trim(trendName)) {
-      //            alert("Trend name is empty");
-      //            return false;
-      //        }
       var trendId;
       console.log(JSON.stringify({trend: { name: trendName, user_id: "1" }}));
-      var jqxhr = $.ajax({
-        type: "POST",
-        url: "http://localhost:3000/api/trends",
-        headers: {'X-Auth-Token' : authToken },
-        data: JSON.stringify({trend: { name: trendName, user_id: "1" }}),
-        contentType: "application/json; charset=UTF-8",
-        success: function(response){
-          trendId = response.id;
-          $('.instance-container').each(function(i, obj){
-            var name = obj.getElementsByTagName('input')[0].value;
-            var startYear = parseInt(obj.getElementsByTagName('input')[1].value);
-            var endYear = parseInt(obj.getElementsByTagName('input')[2].value);
-            console.log(JSON.stringify({occurrence: { name: name, startyear: startYear, endyear: endYear, trend_id: trendId}}));
-            var jqxhr2 = $.ajax({
-              type: "POST",
-              url: "http://localhost:3000/api/trends/" + trendId + "/occurrences",
-              headers: {'X-Auth-Token' : authToken },
-              data: JSON.stringify({occurrence: { name: name, startyear: startYear, endyear: endYear, trend_id: trendId}}),
-              contentType: "application/json; charset=UTF-8"
+      if (vm.validateName(trendName)) {
+        var jqxhr = $.ajax({
+          type: "POST",
+          url: "http://localhost:3000/api/trends",
+          headers: {'X-Auth-Token' : authToken },
+          data: JSON.stringify({trend: { name: trendName, user_id: "1" }}),
+          contentType: "application/json; charset=UTF-8",
+          success: function(response){
+            trendId = response.id;
+            $('.instance-container').each(function(i, obj){
+              var name = obj.getElementsByTagName('input')[0].value;
+              var startYear = parseInt(obj.getElementsByTagName('input')[1].value);
+              var endYear = parseInt(obj.getElementsByTagName('input')[2].value);
+              if (vm.validateNum(endYear) && vm.validateNum(startYear) && vm.validateName(name)) {
+                console.log(JSON.stringify({occurrence: { name: name, startyear: startYear, endyear: endYear, trend_id: trendId}}));
+                var jqxhr2 = $.ajax({
+                  type: "POST",
+                  url: "http://localhost:3000/api/trends/" + trendId + "/occurrences",
+                  headers: {'X-Auth-Token' : authToken },
+                  data: JSON.stringify({occurrence: { name: name, startyear: startYear, endyear: endYear, trend_id: trendId}}),
+                  contentType: "application/json; charset=UTF-8"
+                })
+                console.log(jqxhr2);
+              }
             })
-            console.log(jqxhr2);
-          })
-        }
-      })
-      console.log(jqxhr);
+          }
+        })
+        console.log(jqxhr);
+      }
     }
   },
   components: {
